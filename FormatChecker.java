3fb42392742d3de73a28a747fb1310485dbef446
GeorgiaHenson <georgiahenson@u.boisestate.edu>
2024-01-30 23:32:01 -0700

    update

--- FormatChecker.java
+++ FormatChecker.java
@@ -5,22 +5,43 @@ import java.util.Scanner;
 public class FormatChecker {
 
     public FormatChecker(String fileName)
-            throws FileNotFoundException, IncorrectRowCountException, IncorrectColumnCountException {
+            throws FileNotFoundException, IncorrectRowCountException, IncorrectColumnCountException,
+            IncorrectFirstLineException {
 
         Scanner fileScan = new Scanner(new File(fileName));
 
-        int rows = fileScan.nextInt();
-        int cols = fileScan.nextInt();
+        String rowColLine = fileScan.nextLine();
+        Scanner rowColLineScan = new Scanner(rowColLine);
+        int rows = Integer.parseInt(rowColLineScan.next());
+        int cols = Integer.parseInt(rowColLineScan.next());
+        if (rowColLineScan.hasNext()) {
+            rowColLineScan.close();
+            throw new IncorrectFirstLineException("Extra integer on first line");
+        } else {
+            rowColLineScan.close();
+        }
 
         int rowCounter = 0;
-        int colCounter = 0;
+
+        
 
         while (fileScan.hasNextLine()) {
+            String line = fileScan.nextLine();
+            Scanner lineScan = new Scanner(line);
             rowCounter++;
-            while (fileScan.hasNext()) {
+            int colCounter = 0;
+            while (lineScan.hasNext()) {
                 colCounter++;
-                double tester = fileScan.nextDouble();
+                double tester = Double.parseDouble(lineScan.next());
             }
+            lineScan.close();
+            if (colCounter < cols) {
+
+                throw new IncorrectColumnCountException("Not enough columns in file");
+            } else if (colCounter > cols) {
+                throw new IncorrectColumnCountException("Too many columns in file");
+            }
+
         }
         if (rowCounter < rows) {
             throw new IncorrectRowCountException("Not enough rows in file");
@@ -28,36 +49,37 @@ public class FormatChecker {
             throw new IncorrectRowCountException("Too many rows in file");
         }
 
-        if (colCounter < cols) {
-            throw new IncorrectColumnCountException("Not enough columns in file");
-        } else if (colCounter > cols) {
-            throw new IncorrectColumnCountException("Not enough columns in file");
-        }
-
     }
 
     public static void main(String[] args) {
-        if (args.length > 1) {
-            for (String x : args) {
+        if (args.length > 0) {
+            for (var x : args) {
                 try {
                     System.out.println(x);
                     FormatChecker checkFormat = new FormatChecker(x);
                     System.out.println("VALID");
+
                 } catch (FileNotFoundException e) {
-                    System.out.println(e.getMessage());
+                    System.out.println(e);
+
                     System.out.println("INVALID");
                 } catch (IncorrectColumnCountException e) {
-                    System.out.println(e.getMessage());
+                    System.out.println(e);
+
                     System.out.println("INVALID");
                 } catch (IncorrectRowCountException e) {
-                    System.out.println(e.getMessage());
+                    System.out.println(e);
+
                     System.out.println("INVALID");
                 } catch (Exception e) {
-                    System.out.println(e.getMessage());
+                    System.out.println(e);
+
                     System.out.println("INVALID");
                 } finally {
                     System.out.println();
+
                 }
+
             }
 
         } else {
@@ -65,5 +87,6 @@ public class FormatChecker {
                     "Usage:$ java FormatChecker file1 [file2 ... fileN]\n where the names of one or more input files are given.");
             return;
         }
+
     }
 }
---
+++ IncorrectFirstLineException.java
@@ -0,0 +1,6 @@
+public class IncorrectFirstLineException extends Exception{
+   
+    public IncorrectFirstLineException(String errorstring){
+        super(errorstring);
+    }
+}
